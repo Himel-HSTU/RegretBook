a877b60b3c8522287dc32a1a0e1673d567d50898
Himel-HSTU <mdhabiburrahmanhimel27@gmail.com>
2026-09-22 02:33:49 +0600

    Just Warm Up, Added First 3 User Story

---
+++ RegretBook/RegretBook.Domain/Entities/Regret.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegretBook.Domain.Entities
+{
+    public class Regret
+    {
+        private Guid _id;
+        private Guid _userId;
+        private string _description;
+        private DateTime _createdAt;
+
+        public Regret()
+        {
+            _id = Guid.NewGuid();
+            _createdAt = DateTime.UtcNow;
+        }
+
+        public Guid Id { get { return _id; } }
+        public Guid UserId { get { return _userId; } set { _userId = value; } }
+        public string Description { get { return _description; } set {  _description = value; } }
+        public DateTime CreatedAt { get { return _createdAt; } }
+    }
+}
