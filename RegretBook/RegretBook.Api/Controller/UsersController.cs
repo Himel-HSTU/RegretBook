b7c19ea6f5dd286b7382b8a21985075c29a2f9c6
Himel-HSTU <mdhabiburrahmanhimel27@gmail.com>
2026-09-26 21:10:39 +0600

    Worked on User Controller Also Checked API Via Swagger and Postman

--- RegretBook/RegretBook.Api/Controller/UsersController.cs
+++ RegretBook/RegretBook.Api/Controller/UsersController.cs
@@ -1,72 +1,99 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using RegretBook.Api.Dtos;
+using RegretBook.Api.Data;
+using RegretBook.Domain.Entities;
 namespace TwitterClone.Api.Controllers
 {
 
     // api/users
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class UsersController : ControllerBase
     {
 
-        public UsersController() { }
+        private readonly UserRepository _userRepository;
+
+        public UsersController(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
 
 
         // /api/users
         [HttpGet]
+        [AllowAnonymous]
         public IActionResult GetUsers()
         {
-            return Ok(new List<object>
-            {
-                new
-                {
-                    UserId = Guid.NewGuid(),
-                    UserName = "user1",
-                },
-                new
-                {
-                    UserId = Guid.NewGuid(),
-                    UserName = "user2",
-                },
-            });
+
+            //var userRepository = new UserRepository();
+
+            List<User> users = _userRepository.GetUsers();
+
+            return Ok(users);
         }
 
         // /api/users
         [HttpPost]
         [AllowAnonymous]
-        public IActionResult CreateUser()
+        public IActionResult CreateUser([FromBody] CreateUserDto createUserDto)
         {
-            return Ok(new
+
+            if (string.IsNullOrWhiteSpace(createUserDto.FirstName) || string.IsNullOrWhiteSpace(createUserDto.LastName) || string.IsNullOrWhiteSpace(createUserDto.Email))
             {
-                UserId = Guid.NewGuid(),
-                UserName = "newuser",
+                return BadRequest("All fields are required to create a new user.");
+            }
+
+            var existingUser = _userRepository.GetUserByEmail(createUserDto.Email);
+            if (existingUser != null)
+            {
+                return BadRequest("A user with this email already exists.");
+            }
+
+            //var userRepository = new UserRepository();
+
+            var createdUser = _userRepository.AddUser(new User
+            {
+                FirstName = createUserDto.FirstName,
+                LastName = createUserDto.LastName,
+                Email = createUserDto.Email
             });
+
+            return Ok(createdUser);
         }
 
 
         // /api/users/{id}
         [HttpGet("{id}")]
+        [AllowAnonymous]
         public IActionResult GetUserById([FromRoute] Guid id)
         {
-            return Ok(new
-            {
-                UserId = id,
-                UserName = "user" + id.ToString(),
-            });
-        }
 
+            var user = _userRepository.GetUserById(id);
+            if (user==null)
+            {
+                return NotFound($"User with ID {id} not found.");
+            }
+            return Ok(user);
+        }
 
         // PUT /api/users/{id}
         [HttpPut("{id}")]
-        public IActionResult UpdateUser([FromRoute] Guid id)
+        
+        public IActionResult UpdateUser([FromRoute] Guid id, [FromBody] UpdateUserDtos updateUserDto)
         {
-            return Ok(new
+
+            var user = _userRepository.GetUserById(id);
+            if (user==null)
             {
-                UserId = id,
-                UserName = "updateduser" + id.ToString(),
-            });
+                return NotFound($"User with ID {id} not found.");
+            }
+            user.FirstName = updateUserDto.FirstName ?? user.FirstName;
+            user.LastName = updateUserDto.LastName ?? user.LastName;
+            // jodi name update dei tahole update hobe otherwise purono name thakbe
+            _userRepository.UpdateUser(user);
+            return Ok(user);
         }
 
 
@@ -82,11 +109,14 @@ namespace TwitterClone.Api.Controllers
         [HttpDelete("{id}")]
         public IActionResult DeleteUser([FromRoute] Guid id)
         {
-            return Ok(new
+            var user = _userRepository.GetUserById(id);
+            if (user == null)
             {
-                UserId = id,
-                Message = "User deleted successfully.",
-            });
+                return NotFound($"User with ID {id} not found.");
+            }
+            var isdeleted = _userRepository.DeleteUser(id);
+            return Ok(isdeleted);   
         }
+
     }
 }
