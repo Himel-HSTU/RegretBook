68b7d15a5fc30e20a0e64c4defb43abbb3abf4fe
Himel-HSTU <mdhabiburrahmanhimel27@gmail.com>
2026-09-26 16:20:11 +0600

    Worked on WEB API Controller

---
+++ RegretBook/RegretBook.Api/Controller/LikesController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TwitterClone.Api.Controllers
+{
+
+    // api/likes
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikesController : ControllerBase
+    {
+
+        public LikesController() { }
+
+
+        // GET /api/likes?userId={userId}&regretId={regretId}
+        [HttpGet]
+        public IActionResult GetLikes([FromQuery] Guid? userId, [FromQuery] Guid? regretId)
+        {
+            return Ok(new List<object>
+            {
+                new
+                {
+                    LikeId = Guid.NewGuid(),
+                    UserId = userId ?? Guid.NewGuid(),
+                    regretId = regretId ?? Guid.NewGuid(),
+                    CreatedAt = DateTime.UtcNow.AddMinutes(-30),
+                },
+                new
+                {
+                    LikeId = Guid.NewGuid(),
+                    UserId = userId ?? Guid.NewGuid(),
+                    regretId = regretId ?? Guid.NewGuid(),
+                    CreatedAt = DateTime.UtcNow.AddMinutes(-10),
+                },
+            });
+        }
+
+        // GET /api/likes/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetLikeById([FromRoute] Guid id)
+        {
+            return Ok(new
+            {
+                LikeId = id,
+                UserId = Guid.NewGuid(),
+                regretId = Guid.NewGuid(),
+                CreatedAt = DateTime.UtcNow,
+            });
+        }
+
+        // POST /api/likes
+        [HttpPost]
+        public IActionResult CreateLike()
+        {
+            return Ok(new
+            {
+                LikeId = Guid.NewGuid(),
+                UserId = Guid.NewGuid(),
+                regretId = Guid.NewGuid(),
+                CreatedAt = DateTime.UtcNow,
+            });
+        }
+
+        // DELETE /api/likes/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLike([FromRoute] Guid id)
+        {
+            return Ok(new
+            {
+                LikeId = id,
+                Message = "Like removed successfully.",
+            });
+        }
+    }
+}
---
+++ RegretBook/RegretBook.Api/Controller/ReregretController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TwitterClone.Api.Controllers
+{
+
+    // api/reregrets
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReregretController : ControllerBase
+    {
+
+        public ReregretController() { }
+
+
+        // GET /api/reregrets?userId={userId}&regretId={regretId}
+        [HttpGet]
+        public IActionResult GetReregrets([FromQuery] Guid? userId, [FromQuery] Guid? regretId)
+        {
+            return Ok(new List<object>
+            {
+                new
+                {
+                    ReregretId = Guid.NewGuid(),
+                    UserId = userId ?? Guid.NewGuid(),
+                    RegretId = regretId ?? Guid.NewGuid(),
+                    Comment = "Great point!",
+                    CreatedAt = DateTime.UtcNow.AddHours(-3),
+                },
+                new
+                {
+                    ReregretId = Guid.NewGuid(),
+                    UserId = userId ?? Guid.NewGuid(),
+                    RegretId = regretId ?? Guid.NewGuid(),
+                    Comment = "",
+                    CreatedAt = DateTime.UtcNow.AddHours(-1),
+                },
+            });
+        }
+
+        // GET /api/reregrets/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetReregretById([FromRoute] Guid id)
+        {
+            return Ok(new
+            {
+                ReregretId = id,
+                UserId = Guid.NewGuid(),
+                RegretId = Guid.NewGuid(),
+                Comment = "reregret" + id.ToString(),
+                CreatedAt = DateTime.UtcNow,
+            });
+        }
+
+        // POST /api/reregrets
+        [HttpPost]
+        public IActionResult CreateReregret()
+        {
+            return Ok(new
+            {
+                ReregretId = Guid.NewGuid(),
+                UserId = Guid.NewGuid(),
+                RegretId = Guid.NewGuid(),
+                Comment = "New reregret comment.",
+                CreatedAt = DateTime.UtcNow,
+            });
+        }
+
+        // PUT /api/reregrets/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateReregret([FromRoute] Guid id)
+        {
+            return Ok(new
+            {
+                ReregretId = id,
+                UserId = Guid.NewGuid(),
+                RegretId = Guid.NewGuid(),
+                Comment = "updatedcomment" + id.ToString(),
+                ModifiedAt = DateTime.UtcNow,
+            });
+        }
+
+        // DELETE /api/reregrets/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteReregret([FromRoute] Guid id)
+        {
+            return Ok(new
+            {
+                ReregretId = id,
+                Message = "Reregret removed successfully.",
+            });
+        }
+    }
+}
